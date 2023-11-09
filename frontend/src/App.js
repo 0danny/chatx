8a6e4b46e79c02b5248e3e56459f4e8bb8f9ba06
0xdanny <dpaolone03@gmail.com>
2023-11-09 18:12:11 +1100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import CreateRoom from "./CreateRoom"
+import JoinRoom from "./JoinRoom"
+import Chat from "./Chat"
+
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
+import "react-tabs/style/react-tabs.css"
+
+function App() {
+    const [dataChannel, setDataChannel] = useState(null)
+
+    const handleDataChannel = (channel) => {
+        setDataChannel(channel)
+    }
+
+    return (
+        <div className="app">
+            <Tabs style={{ width: "100%" }}>
+                <TabList>
+                    <Tab>Host</Tab>
+                    <Tab>Client</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <CreateRoom setDataChannel={setDataChannel} />
+                </TabPanel>
+
+                <TabPanel>
+                    <JoinRoom setDataChannel={setDataChannel} />
+                </TabPanel>
+            </Tabs>
+
+            <div className="seperator" style={{ marginTop: "20px" }}></div>
+
+            <Chat dataChannel={dataChannel} />
+        </div>
+    )
+}
+
+export default App
